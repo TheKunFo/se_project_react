a5950ef7f7e7afddb0302a3b2b3f680102077769
TheKunFo <thekunfo@icloud.com>
2025-06-23 09:35:20 -0500

    Update latitude and longitude constants for accurate location data

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
 export const APIkey = "4f4a7c684ab284cce2d16b9adbedee0d";
-export const latitude = "32.806275208111934";
-export const longitude = "-96.5281044467297";
+export const latitude = "32.75518448517437";
+export const longitude = "-96.47282948610665";
 export const BASE_URL_BACKEND = "http://127.0.0.1:3001/";
